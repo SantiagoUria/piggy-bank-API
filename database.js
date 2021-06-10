d432d1c83b695c29d630f8f6bd1675df95878917
SantiagoUria <67654994+SantiagoUria@users.noreply.github.com>
2021-06-10 20:44:02 -0300

    delete comments

--- database.js
+++ database.js
@@ -7,7 +7,6 @@ const mysqlConnection = mysql.createConnection({
   database: "heroku_2a6a2868cc826c7",
   dateStrings: true
 });
-// mysql://b47e102375ae56:7409e9c3@us-cdbr-east-04.cleardb.com/heroku_2a6a2868cc826c7?reconnect=true
 
 mysqlConnection.connect((err) => {
   err ? console.log(err) : console.log("the database connection was successful");
