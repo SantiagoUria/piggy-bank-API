f5f1eb1c1af7e6adcfb21389248594b8afe80ee7
SantiagoUria <67654994+SantiagoUria@users.noreply.github.com>
2021-06-10 17:39:42 -0300

    change database connection config

--- database.js
+++ database.js
@@ -2,12 +2,12 @@ const mysql = require("mysql");
 
 const mysqlConnection = mysql.createConnection({
   host: "us-cdbr-east-04.cleardb.com",
-  user: "b9b3531f73b6ae",
-  password: "cfacf22c",
-  database: "heroku_e04fcd6f50c88cf",
+  user: "b47e102375ae56",
+  password: "7409e9c3",
+  database: "heroku_2a6a2868cc826c7",
   dateStrings: true
 });
-// mysql://b9b3531f73b6ae:cfacf22c@us-cdbr-east-04.cleardb.com/heroku_e04fcd6f50c88cf?reconnect=true
+// mysql://b47e102375ae56:7409e9c3@us-cdbr-east-04.cleardb.com/heroku_2a6a2868cc826c7?reconnect=true
 
 mysqlConnection.connect((err) => {
   err ? console.log(err) : console.log("the database connection was successful");
